ae2adcd191ed6196729b2c5269b2ef39d7333cbd
Shi Yan <billconan@gmail.com>
2019-05-15 23:58:07 -0700

    set base url

--- src/pyodide.js
+++ src/pyodide.js
@@ -6,6 +6,7 @@ var languagePluginLoader = new Promise((resolve, reject) => {
   // This is filled in by the Makefile to be either a local file or the
   // deployed location. TODO: This should be done in a less hacky
   // way.
+  self.languagePluginUrl = "/pyodide/build/"
   var baseURL = self.languagePluginUrl || '{{DEPLOY}}';
   baseURL = baseURL.substr(0, baseURL.lastIndexOf('/')) + '/';
 
